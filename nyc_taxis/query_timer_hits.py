75f18f879b078afa060a219370efc77a753b778c
Kiran Prakash <awskiran@amazon.com>
2023-08-14 18:06:35 -0700

    run-for-all-cache-types

--- nyc_taxis/query_timer_hits.py
+++ nyc_taxis/query_timer_hits.py
@@ -139,20 +139,17 @@ def clearcache(args):
     else:
         print("Failed to clear request cache." + str(response.status_code))
 
-def main():
-    parser = argparse.ArgumentParser(description='OpenSearch Query Response Time Plotter')
-    parser.add_argument('--endpoint', help='OpenSearch domain endpoint (https://example.com)')
-    parser.add_argument('--username', help='Username for authentication')
-    parser.add_argument('--password', help='Password for authentication')
-    parser.add_argument('--days',     help='Number of days in the range to keep increasing to')
-    parser.add_argument('--cache',    help='True for cache enabled and false otherwise, defaults to FALSE.', default='false')
-    parser.add_argument('--type', help='Type of cache we are using, for logging purposes')
-    parser.add_argument('--webhook', help='Slack webhook for notifying when the script is finished.')
-    args = parser.parse_args()
 
+def process_cache_type(args, cache_type):
     # Get baseline hit count
     data = get_request_cache_stats(args.endpoint, args.username, args.password)
     hit_count = next(iter(data['nodes'].values()))['indices']['request_cache']['hit_count']
+
+    # Format the datetime as a string for the filename
+    formatted_datetime = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Create a filename using the formatted datetime
+    filename = f"results_{formatted_datetime}_{cache_type}.csv"
 
     num_queries = 250 # Number of times to execute the query for each date range
     save_path = 'results/'  # Path to save results
@@ -165,26 +162,17 @@ def main():
     daily_medians = []
     daily_mins = []
     daily_max = []
-
-    # Get the current date and time
-    current_datetime = datetime.datetime.now()
-
-    # Format the datetime as a string for the filename
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
-
-    # Create a filename using the formatted datetime
-    filename = f"results_{formatted_datetime}.csv"
-
-    # Execute the query multiple times and measure the response time
     for day in range(1, int(args.days) + 1):
-        clearcache(args) # clear cache to start
+        clearcache(args)  # clear cache to start
         print(f"Starting iterations for range: Jan 1 00:00:00 to Jan {day} 11:59:59")
         response_times = []
         for x in range(1, num_queries + 1):
-            response_time = send_query_and_measure_time(day, hit_count, args.endpoint, args.username, args.password, args.cache) # Get took time for query
-            new_hits = next(iter(get_request_cache_stats(args.endpoint, args.username, args.password)['nodes'].values()))['indices']['request_cache']['hit_count'] # Check new number of hits
+            response_time = send_query_and_measure_time(day, hit_count, args.endpoint, args.username, args.password,
+                                                        args.cache)  # Get took time for query
+            new_hits = next(iter(get_request_cache_stats(args.endpoint, args.username, args.password)['nodes'].values()))[
+                'indices']['request_cache']['hit_count']  # Check new number of hits
 
-            if new_hits > hit_count: # If hit count increased
+            if new_hits > hit_count:  # If hit count increased
                 print(f"Hit. Took time: {response_time}")
                 hit_count = new_hits
                 isHit = True
@@ -193,16 +181,16 @@ def main():
                 print(f"Miss. Took time: {response_time}")
                 isHit = False
 
-            # Append a tuple with response time and hit/miss status
-            response_times.append(response_time)
-            print(f"Response {x}/{num_queries} received.")
+        # Append a tuple with response time and hit/miss status
+        response_times.append(response_time)
+        print(f"Response {x}/{num_queries} received.")
 
         median = np.median(response_times[1:])
-        average_response_time = sum(response_times[1:]) / (num_queries - 1) # Average response time for num_queries - 1 hits (first was a miss before it got written to the cache)
-        p99_latency = np.percentile(response_times[1:], 99) # Calculate p99 latency
-        p95_latency = np.percentile(response_times[1:], 95) # Calculate p95
-        p90_latency = np.percentile(response_times[1:], 90) # Calculate p90
-
+        average_response_time = sum(response_times[1:]) / (
+                    num_queries - 1)  # Average response time for num_queries - 1 hits (first was a miss before it got written to the cache)
+        p99_latency = np.percentile(response_times[1:], 99)  # Calculate p99 latency
+        p95_latency = np.percentile(response_times[1:], 95)  # Calculate p95
+        p90_latency = np.percentile(response_times[1:], 90)  # Calculate p90
 
         # Collect the data
         daily_averages.append(average_response_time)
@@ -212,9 +200,9 @@ def main():
         daily_medians.append(median)
         daily_mins.append(min(response_times[1:]))
         daily_max.append(max(response_times[1:]))
-        
+
         with open(save_path + filename, 'a') as csv_file:
-            csv_file.write(f'Jan 1 to Jan {str(day)} using cache of type: {args.type} \n')
+            csv_file.write(f'Jan 1 to Jan {str(day)} using cache of type: {cache_type} \n')
             for value in response_times:
                 csv_file.write(str(value) + '\n')
             csv_file.write(f"Average response time: {average_response_time} \n")
@@ -225,22 +213,44 @@ def main():
             csv_file.write(f"Minimum: {min(response_times[1:])} \n ")
             csv_file.write(f"Maximum: {max(response_times[1:])} \n ")
             csv_file.write("\n")
-        
-        print(f"Results for Jan 1 to Jan {str(day)} appended to {filename}.")
 
-    # print items in tabular
-    print(f"Results for cache of type {args.type}")
-    print("All average response times: ")
-    for avg_time in enumerate(daily_averages, start=1):
-        print(f"{avg_time}")
+            print(f"Results for Jan 1 to Jan {str(day)} appended to {filename}.")
 
-    print("All Miss took times: ")
-    for miss_took_time in enumerate(miss_took_times, start=1):
-        print(f"{miss_took_time}")
+        # print items in tabular
+        print(f"Results for cache of type {cache_type}")
+        print("All average response times: ")
+        for avg_time in enumerate(daily_averages, start=1):
+            print(f"{avg_time}")
 
-    print("All p90 response times:")
-    for daily_p90_latency in enumerate(daily_p90_latencies, start=1):
-        print(f"{daily_p90_latency}")
+        print("All Miss took times: ")
+        for miss_took_time in enumerate(miss_took_times, start=1):
+            print(f"{miss_took_time}")
+
+        print("All p90 response times:")
+        for daily_p90_latency in enumerate(daily_p90_latencies, start=1):
+            print(f"{daily_p90_latency}")
+
+def main():
+    parser = argparse.ArgumentParser(description='OpenSearch Query Response Time Plotter')
+    parser.add_argument('--endpoint', help='OpenSearch domain endpoint (https://example.com)')
+    parser.add_argument('--username', help='Username for authentication')
+    parser.add_argument('--password', help='Password for authentication')
+    parser.add_argument('--days',     help='Number of days in the range to keep increasing to')
+    parser.add_argument('--cache',    help='True for cache enabled and false otherwise, defaults to FALSE.', default='true')
+    parser.add_argument('--type',     help='Type of cache we are using, for logging purposes', default='all')
+    parser.add_argument('--webhook',  help='Slack webhook for notifying when the script is finished.')
+    args = parser.parse_args()
+
+    caches = ['diskOnly', 'diskAndHeap', 'ehcache_heap_only', 'os_cache_only']
+
+    if args.type != 'all':
+        caches = [args.type]
+
+    for cache_type in caches:# Execute the query multiple times and measure the response time
+        start_time = time.time()
+        process_cache_type(args, cache_type)
+        elapsed_time = (time.time() - start_time) / 60
+        print(f"Time take for cache_type {cache_type} : {elapsed_time} minutes")
 
     send_slack_notification(args.webhook, args.type)
 
