12c195ff886c1aeb84c744e8a9d1ac13cb62ae08
Peter Alfonsi <petealft@amazon.com>
2023-11-28 10:10:08 -0800

    added newline for readabilty

--- modified_nyc_taxis/workload.py
+++ modified_nyc_taxis/workload.py
@@ -132,7 +132,7 @@ def expensive_1(workload, params, **kwargs):
     # as a test only, write these vals to file so we can check the logic allows caching 
     with open("expensive_1_values.txt", "a+") as f: 
         for val_dict in vals: 
-            f.write(json.dumps(val_dict))
+            f.write(json.dumps(val_dict) + "\n")
     return {
         "body": {
             "size": 0,
