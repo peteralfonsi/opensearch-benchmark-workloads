7e76c87da68afdbfb7b611d1ad675f681b2a25af
Peter Alfonsi <petealft@amazon.com>
2023-12-14 15:51:26 -0800

    Setup scenario 8

--- modified_nyc_taxis/query_value_providers.py
+++ modified_nyc_taxis/query_value_providers.py
@@ -5,92 +5,85 @@ import datetime
 
 # common helper functions 
 
-def random_money_values(max_value): 
-    gte_cents = random.randrange(0, max_value*100)
-    lte_cents = random.randrange(gte_cents, max_value*100)
-    return {
-        "gte":gte_cents/100,
-        "lte":lte_cents/100
-    }
-
 def format_date(datetime_obj): 
     return datetime_obj.strftime("%Y-%m-%d %H:%M:%S") # check this
 
-def random_dates(min_value, max_value): 
-    # arguments are datetime objects
-    min_timestamp = datetime.datetime.timestamp(min_value) 
-    max_timestamp = datetime.datetime.timestamp(max_value) 
-    diff = max_timestamp - min_timestamp
-    gte_fraction = random.uniform(0, 1)
-    lte_fraction = random.uniform(gte_fraction, 1.0)
+def random_dates_with_constant_range(separation_days): 
+    jan1 = datetime.datetime(2015, 1, 1)
+    dec31 = datetime.datetime(2015, 1, 15)
+    min_lowerbound_timestamp = datetime.datetime.timestamp(jan1) # timestamps are in seconds, not ns
+    max_lowerbound_timestamp = datetime.datetime.timestamp(dec31) - (24 * 3600 * separation_days)
+    lowerbound_fraction = random.uniform(0, 1)
+    diff = max_lowerbound_timestamp - min_lowerbound_timestamp
+    base_lowerbound_timestamp = min_lowerbound_timestamp + int(lowerbound_fraction * diff)
+    base_upperbound_timestamp = base_lowerbound_timestamp + (24 * 3600 * separation_days)
 
-    gte_date = datetime.datetime.fromtimestamp(min_timestamp + int(gte_fraction * diff))
-    lte_date = datetime.datetime.fromtimestamp(min_timestamp + int(lte_fraction * diff))
+    fuzz_max_seconds = 3600 # one hour of fuzz in either direction, to make extra sure we dont get unwanted collisions
+    lowerbound_fuzz = random.uniform(-fuzz_max_seconds, fuzz_max_seconds)
+    upperbound_fuzz = random.uniform(-fuzz_max_seconds, fuzz_max_seconds)
+
+    lowerbound_date = datetime.datetime.fromtimestamp(base_lowerbound_timestamp + lowerbound_fuzz) 
+    upperbound_date = datetime.datetime.fromtimestamp(base_upperbound_timestamp + upperbound_fuzz)
+
     return {
-        "gte":format_date(gte_date), 
-        "lte":format_date(lte_date)
+        "gte":format_date(lowerbound_date), 
+        "lte":format_date(upperbound_date)
     }
 
 
 # actual provider functions
 
-def cheap_passenger_count_provider(): 
-    gte = random.randrange(1, 5) # assuming passenger count is 1 to 4?
-    lte = random.randrange(gte, 5)
-    return {
-        "gte": gte, 
-        "lte": lte
-    }
+def ps_1w_provider(): 
+    return random_dates_with_constant_range(7)
 
-def cheap_tip_amount_provider(): 
-    # random dollar + cents values between 0.00 and 10.99
-    return random_money_values(10.99)
-
-def cheap_fare_amount_provider(): 
-    # random dollar + cents values between 0.00 and 100.99
-    return random_money_values(100.99)
-
-def cheap_total_amount_provider(): 
-    return random_money_values(111.98)
-
-def cheap_pickup_provider(): 
-    # random days between 1/1/2015 and 12/31/2015
-    return random_dates(datetime.datetime(2015, 1, 1), datetime.datetime(2015, 1, 15))
-
-def cheap_dropoff_provider(): 
-    # random days between 1/1/2015 and 12/31/2015
-    # Should be different from pickup, otherwise we will use the same values 
-    # for cached queries which have both a pickup and dropoff field, which would give a range of 0 -> 
-    # non-representative results 
-    return random_dates(datetime.datetime(2015, 1, 1), datetime.datetime(2015, 1, 15))
-
-def cheap_distance_provider(): 
-    gte = random.randint(0, 10)
-    lte = random.randint(gte, 20)
-    return {"gte":gte, "lte":lte}
+def ps_2w_provider(): 
+    return random_dates_with_constant_range(14)
     
+def ps_3w_provider(): 
+    return random_dates_with_constant_range(21)
+
+def ps_1m_provider(): 
+    return random_dates_with_constant_range(30)
+
+def ps_6w_provider(): 
+    return random_dates_with_constant_range(45)
+
+def ps_2m_provider(): 
+    return random_dates_with_constant_range(60)
+
+def ps_3m_provider(): 
+    return random_dates_with_constant_range(90)
+
+def ps_4m_provider(): 
+    return random_dates_with_constant_range(120)
+
+def ps_6m_provider(): 
+    return random_dates_with_constant_range(180)
 
 
 # name for each specific query
 fn_names = [
-    "cheap_passenger_count",
-    "cheap_tip_amount",
-    "cheap_fare_amount",
-    "cheap_total_amount", 
-    "cheap_pickup",
-    "cheap_dropoff", 
-    "cheap_distance"
+    "ps_1w", 
+    "ps_2w", 
+    "ps_3w", 
+    "ps_1m", 
+    "ps_6w", 
+    "ps_2m", 
+    "ps_3m", 
+    "ps_4m", 
+    "ps_6m"
 ]
 
 # the value generator for each specific query
 fn_value_generators = {
-    "cheap_passenger_count":cheap_passenger_count_provider,
-    "cheap_tip_amount":cheap_tip_amount_provider,
-    "cheap_fare_amount":cheap_fare_amount_provider,
-    "cheap_total_amount":cheap_total_amount_provider, 
-    "cheap_pickup":cheap_pickup_provider,
-    "cheap_dropoff":cheap_dropoff_provider,
-    "cheap_distance":cheap_distance_provider,
-    "cheap_distance":cheap_distance_provider
+    "ps_1w":ps_1w_provider, 
+    "ps_2w":ps_2w_provider, 
+    "ps_3w":ps_3w_provider, 
+    "ps_1m":ps_1m_provider, 
+    "ps_6w":ps_6w_provider, 
+    "ps_2m":ps_2m_provider, 
+    "ps_3m":ps_3m_provider, 
+    "ps_4m":ps_4m_provider, 
+    "ps_6m":ps_6m_provider
 } 
 
--- modified_nyc_taxis/workload.py
+++ modified_nyc_taxis/workload.py
@@ -21,19 +21,7 @@ for fn_name in fn_names:
         raise Exception("Must generate standard values for {} using generate_standard_random_values.py!".format(fn_name))
 
 # Make the list of all query types so we can keep track of counters for each
-query_types = fn_names + [
-    "expensive_distance_amount_agg",
-    "expensive_autohisto_agg",
-    "expensive_date_histogram_agg",
-    "expensive_date_histogram_calendar_interval",
-    "expensive_date_histogram_calendar_interval_with_tz",
-    "expensive_date_histogram_fixed_interval",
-    "expensive_date_histogram_fixed_interval_with_tz",
-    "expensive_date_histogram_fixed_interval_with_metrics",
-    "expensive_auto_date_histogram",
-    "expensive_auto_date_histogram_with_tz",
-    "expensive_auto_date_histogram_with_metrics"
-]
+query_types = fn_names
 query_type_counters = {} # keeps track of how many times we have pulled from the standard values, for each query type
 
 for query_type in query_types: 
@@ -80,350 +68,55 @@ def get_basic_range_query(field, gte, lte):
 # which lives in query_value_providers.py. 
 # This provider fn is also used to generate standard random values in the first place. 
 
-def cheap_passenger_count(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_passenger_count"], "cheap_passenger_count")[0]
-    # based on random_passenger_count from https://github.com/kiranprakash154/opensearch-benchmark-workloads/blob/kp/custom-workload/nyc_taxis/workload.py
-    return get_basic_range_query("passenger_count", val_dict["gte"], val_dict["lte"])
-
-def cheap_tip_amount(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_tip_amount"], "cheap_tip_amount")[0]
-    return get_basic_range_query("tip_amount", val_dict["gte"], val_dict["lte"])
-
-
-def cheap_fare_amount(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_fare_amount"], "cheap_fare_amount")[0]
-    return get_basic_range_query("fare_amount", val_dict["gte"], val_dict["lte"])
-
-def cheap_total_amount(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_total_amount"], "cheap_total_amount")[0]
-    return get_basic_range_query("total_amount", val_dict["gte"], val_dict["lte"])
-
-def cheap_pickup(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_pickup"], "cheap_pickup")[0]
-    return get_basic_range_query("pickup_datetime", val_dict["gte"], val_dict["lte"])
 
 def cheap_dropoff(workload, params, **kwargs): 
     val_dict = get_values(params, ["cheap_dropoff"], "cheap_dropoff")[0]
     return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
+def ps_1w(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_1w"], "ps_1w")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-# The following are randomized versions of the existing agg operations in nyc_taxis/operations/default.json
-def expensive_distance_amount_agg(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_distance"], "expensive_distance_amount_agg")[0]
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "bool": {
-            "filter": {
-              "range": {
-                "trip_distance": {
-                  "lt": val_dict["lte"],
-                  "gte": val_dict["gte"]
-                }
-              }
-            }
-          }
-        },
-        "aggs": {
-          "distance_histo": {
-            "histogram": {
-              "field": "trip_distance",
-              "interval": 1
-            },
-            "aggs": {
-              "total_amount_stats": {
-                "stats": {
-                  "field": "total_amount"
-                }
-              }
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_2w(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_2w"], "ps_2w")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_autohisto_agg(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_autohisto_agg")[0]
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lte": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "auto_date_histogram": {
-              "field": "dropoff_datetime",
-              "buckets": 20
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_3w(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_3w"], "ps_3w")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_date_histogram_agg(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_agg")[0]
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-              "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lte": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "calendar_interval": "day"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_1m(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_1m"], "ps_1m")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_date_histogram_calendar_interval(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_calendar_interval")[0]
-    return {
-        "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "calendar_interval": "month"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_6w(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_6w"], "ps_6w")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_date_histogram_calendar_interval_with_tz(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_calendar_interval_with_tz")[0]
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "calendar_interval": "month",
-              "time_zone": "America/New_York"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_2m(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_2m"], "ps_2m")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_date_histogram_fixed_interval(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_fixed_interval")[0]
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "fixed_interval": "60d"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_3m(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_3m"], "ps_3m")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_date_histogram_fixed_interval_with_tz(workload, params, **kwargs):
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_fixed_interval_with_tz")[0] 
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "fixed_interval": "60d",
-              "time_zone": "America/New_York"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
-def expensive_date_histogram_fixed_interval_with_metrics(workload, params, **kwargs):
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_date_histogram_fixed_interval_with_metrics")[0] 
-    return {
-      "name": "date_histogram_fixed_interval_with_metrics",
-      "operation-type": "search",
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "date_histogram": {
-              "field": "dropoff_datetime",
-              "fixed_interval": "60d"
-            },
-            "aggs": {
-              "total_amount": { "stats": { "field": "total_amount" } },
-              "tip_amount": { "stats": { "field": "tip_amount" } },
-              "trip_distance": { "stats": { "field": "trip_distance" } }
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_4m(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_4m"], "ps_4m")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
-def expensive_auto_date_histogram(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_auto_date_histogram")[0] 
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "auto_date_histogram": {
-              "field": "dropoff_datetime",
-              "buckets": "12"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
-
-def expensive_auto_date_histogram_with_tz(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_auto_date_histogram_with_tz")[0] 
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "auto_date_histogram": {
-              "field": "dropoff_datetime",
-              "buckets": "13",
-              "time_zone": "America/New_York"
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
-
-def expensive_auto_date_histogram_with_metrics(workload, params, **kwargs): 
-    val_dict = get_values(params, ["cheap_dropoff"], "expensive_auto_date_histogram_with_metrics")[0] 
-    return {
-      "body": {
-        "size": 0,
-        "query": {
-          "range": {
-            "dropoff_datetime": {
-              "gte": val_dict["gte"],
-              "lt": val_dict["lte"]
-            }
-          }
-        },
-        "aggs": {
-          "dropoffs_over_time": {
-            "auto_date_histogram": {
-              "field": "dropoff_datetime",
-              "buckets": "12"
-            },
-            "aggs": {
-              "total_amount": { "stats": { "field": "total_amount" } },
-              "tip_amount": { "stats": { "field": "tip_amount" } },
-              "trip_distance": { "stats": { "field": "trip_distance" } }
-            }
-          }
-        }
-      },
-      "index":"nyc_taxis"
-    }
+def ps_6m(workload, params, **kwargs): 
+    val_dict = get_values(params, ["ps_6m"], "ps_6m")[0]
+    return get_basic_range_query("dropoff_datetime", val_dict["gte"], val_dict["lte"])
 
 def register(registry):
-    registry.register_param_source("cheap-passenger-count-param-source", cheap_passenger_count)
-    registry.register_param_source("cheap-tip-amount-param-source", cheap_tip_amount)
-    registry.register_param_source("cheap-fare-amount-param-source", cheap_fare_amount)
-    registry.register_param_source("cheap-total-amount-param-source", cheap_total_amount)
-    registry.register_param_source("cheap-pickup-param-source", cheap_pickup)
-    registry.register_param_source("cheap-dropoff-param-source", cheap_dropoff)
-    registry.register_param_source("expensive-distance-amount-agg-param-source", expensive_distance_amount_agg)
-    registry.register_param_source("expensive-autohisto-agg-param-source", expensive_autohisto_agg)
-    registry.register_param_source("expensive-date-histogram-agg-param-source", expensive_date_histogram_agg)
-    registry.register_param_source("expensive-date-histogram-calendar-interval-param-source", expensive_date_histogram_calendar_interval)
-    registry.register_param_source("expensive-date-histogram-calendar-interval-with-tz-param-source", expensive_date_histogram_calendar_interval_with_tz)
-    registry.register_param_source("expensive-date-histogram-fixed-interval-param-source", expensive_date_histogram_fixed_interval)
-    registry.register_param_source("expensive-date-histogram-fixed-interval-with-tz-param-source", expensive_date_histogram_fixed_interval_with_tz)
-    registry.register_param_source("expensive-date-histogram-fixed-interval-with-metrics-param-source", expensive_date_histogram_fixed_interval_with_metrics)
-    registry.register_param_source("expensive-auto-date-histogram-param-source", expensive_auto_date_histogram)
-    registry.register_param_source("expensive-auto-date-histogram-with-tz-param-source", expensive_auto_date_histogram_with_tz)
-    registry.register_param_source("expensive-auto-date-histogram-with-metrics-param-source", expensive_auto_date_histogram_with_metrics)
+    registry.register_param_source("1w-param-source", ps_1w)
+    registry.register_param_source("2w-param-source", ps_2w)
+    registry.register_param_source("3w-param-source", ps_2w)
+    registry.register_param_source("1m-param-source", ps_1m)
+    registry.register_param_source("6w-param-source", ps_6w)
+    registry.register_param_source("2m-param-source", ps_2m)
+    registry.register_param_source("3m-param-source", ps_3m)
+    registry.register_param_source("4m-param-source", ps_4m)
+    registry.register_param_source("6m-param-source", ps_6m)
     registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
