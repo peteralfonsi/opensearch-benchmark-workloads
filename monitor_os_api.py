194449f299e175c85067079041493575523d1c46
Peter Alfonsi <petealft@amazon.com>
2023-12-15 12:10:45 -0800

    added monitor python file

---
+++ monitor_os_api.py
@@ -0,0 +1,60 @@
+import subprocess
+import json
+import datetime
+import time
+
+node_endpoint = "http://localhost:9200"
+freq = 15 # seconds. increase to 60 later
+num_same_before_stopping = 5 # if cache stats are unchanged (and > 0) for this many iterations, stop running
+
+num_same_counter = 0
+last_heap_entry_number = 0
+
+
+dump_path = "dump"
+out_path_hot_threads = dump_path + "/hot_threads"
+out_path_search_queue = dump_path + "/search_queue"
+out_path_cache_stats = dump_path + "/cache_stats"
+
+def formatted_now(): 
+    return datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+
+def run_hot_threads(): 
+    cmd = "curl -XGET \"{}/_nodes/hot_threads?pretty\"".format(node_endpoint)
+    resp = json.loads(subprocess.run(cmd, shell=True, capture_output=True).stdout)
+    fp = out_path_hot_threads + "/" + formatted_now() + ".json"
+    with open(fp, "w") as f: 
+        json.dump(resp, f)
+
+def run_search_queue(): 
+    cmd = "curl -XGET \"{}/_nodes/thread_pool?pretty\"".format(node_endpoint)
+    resp = json.loads(subprocess.run(cmd, shell=True, capture_output=True).stdout)
+    fp = out_path_search_queue + "/" + formatted_now() + ".json"
+    with open(fp, "w") as f: 
+        json.dump(resp, f)
+
+def do_stop_loop(resp): 
+    node_name = next(iter(resp["nodes"]))
+    rc_info = resp["nodes"][node_name]["indices"]["request_cache"]
+    heap_entries = int(resp["heap"]["entries"])
+    if heap_entries == last_heap_entry_number and heap_entries > 0: 
+        num_same_counter += 1
+    last_heap_entry_number = heap_entries
+    if num_same_counter >= num_same_before_stopping: 
+        return True
+
+def run_cache_stats(): 
+    cmd = "curl -XGET \"{}/_nodes/stats/indices/request_cache?pretty\"".format(node_endpoint)
+    resp = json.loads(subprocess.run(cmd, shell=True, capture_output=True).stdout)
+    fp = out_path_search_queue + "/" + formatted_now() + ".json"
+    with open(fp, "w") as f: 
+        json.dump(resp, f)
+    return resp
+
+while True: 
+    run_hot_threads() 
+    run_search_queue()
+    cache_stats = run_cache_stats()
+    if do_stop_loop(cache_stats): 
+        break
+    time.sleep(freq)
