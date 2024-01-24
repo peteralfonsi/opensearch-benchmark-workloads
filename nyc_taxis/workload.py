84160fdb33c6cdc6769bd38ab9bf36d73c0ce90e
Peter Alfonsi <petealft@amazon.com>
2024-01-24 12:45:13 -0800

    added std value source for total-amount in workload.py

--- nyc_taxis/workload.py
+++ nyc_taxis/workload.py
@@ -1,5 +1,9 @@
 async def delete_snapshot(opensearch, params):
     await opensearch.snapshot.delete(repository=params["repository"], snapshot=params["snapshot"])
 
+def total_amount_std_value_source():
+    return {"gte":1, "lte":4}
+
 def register(registry):
     registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
+    registry.register_standard_value_source("total_amount", total_amount_std_value_source)
