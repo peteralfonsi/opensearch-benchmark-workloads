9740dc26368183e4a0020b1070655ebe3061fedc
Peter Alfonsi <petealft@amazon.com>
2024-01-24 15:36:26 -0800

    adjusted format of dropoff datetime source

--- nyc_taxis/workload.py
+++ nyc_taxis/workload.py
@@ -8,7 +8,8 @@ def trip_distance_std_value_source():
     return {"gte":5, "lte":7}
 
 def dropoff_datetime_std_value_source():
-    return {"gte":"2015-01-01 00:00:00", "lte":"2015-01-04 12:00:00"}
+    #return {"gte":"2015-01-01 00:00:00", "lte":"2015-01-04 12:00:00"}
+    return {"gte":"03/01/2015", "lte":"05/01/2015"}
 
 def register(registry):
     registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
