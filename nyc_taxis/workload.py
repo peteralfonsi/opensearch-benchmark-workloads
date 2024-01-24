c2296e99e5292315560694d00184463b813d9bd2
Peter Alfonsi <petealft@amazon.com>
2024-01-24 15:15:58 -0800

    added dummy sv sources for trip_distance and dropoff_datetime

--- nyc_taxis/workload.py
+++ nyc_taxis/workload.py
@@ -4,6 +4,14 @@ async def delete_snapshot(opensearch, params):
 def total_amount_std_value_source():
     return {"gte":1, "lte":4}
 
+def trip_distance_std_value_source():
+    return {"gte":5, "lte":7}
+
+def dropoff_datetime_std_value_source():
+    return {"gte":"2015-01-01 00:00:00", "lte":"2015-01-04 12:00:00"}
+
 def register(registry):
     registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
     registry.register_standard_value_source("total_amount", total_amount_std_value_source)
+    registry.register_standard_value_source("trip_distance", trip_distance_std_value_source)
+    registry.register_standard_value_source("dropoff_datetime", dropoff_datetime_std_value_source)
