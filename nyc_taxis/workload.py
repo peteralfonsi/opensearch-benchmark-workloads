70b42bdf33c47634641ff5c6ede328f4fd89ada0
Peter Alfonsi <petealft@amazon.com>
2024-01-25 11:27:13 -0800

    register by op and field

--- nyc_taxis/workload.py
+++ nyc_taxis/workload.py
@@ -8,11 +8,12 @@ def trip_distance_std_value_source():
     return {"gte":5, "lte":7}
 
 def dropoff_datetime_std_value_source():
-    return {"gte":"2015-01-01 00:00:00", "lte":"2015-01-04 12:00:00"}
-    #return {"gte":"03/01/2015", "lte":"05/01/2015"}
+    #return {"gte":"2015-01-01 00:00:00", "lte":"2015-01-04 12:00:00"}
+    return {"gte":"03/01/2015", "lte":"05/01/2015", "format":"dd/MM/yyyy"}
 
 def register(registry):
     registry.register_runner("delete-snapshot", delete_snapshot, async_runner=True)
-    registry.register_standard_value_source("total_amount", total_amount_std_value_source)
-    registry.register_standard_value_source("trip_distance", trip_distance_std_value_source)
-    registry.register_standard_value_source("dropoff_datetime", dropoff_datetime_std_value_source)
+    registry.register_standard_value_source("range", "total_amount", total_amount_std_value_source)
+    registry.register_standard_value_source("distance_amount_agg", "trip_distance", trip_distance_std_value_source)
+    registry.register_standard_value_source("autohisto_agg", "dropoff_datetime", dropoff_datetime_std_value_source)
+    registry.register_standard_value_source("date_histogram_agg", "dropoff_datetime", dropoff_datetime_std_value_source)
