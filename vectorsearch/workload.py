e66b6befee30db06e4fd8012de85164bf3c8f73e
Peter Alfonsi <petealft@amazon.com>
2025-01-30 13:24:13 -0800

    first attempt to add randomization
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- vectorsearch/workload.py
+++ vectorsearch/workload.py
@@ -5,7 +5,22 @@
 # compatible open source license.
 
 from .runners import register as register_runners
+from random import uniform
 
+dimensions = 128 
+# TODO: Refine this based on the distribution of base vectors in corpus. 
+min_value = -100 
+max_value = 100
+def vector_source(): 
+    v = [] 
+    for i in range(dimensions): 
+        v.append(uniform(min_value, max_value))
+    return {
+        "vector":v
+    }
 
 def register(registry):
+    # note: hardcoded for field name to be "target_field" (here and in operation definition)
+    registry.register_standard_value_source("randomized-vector-search", "target_field", vector_source)
+    registry.register_query_randomization_info("randomized-vector-search", "knn", [["vector"]], [])
     register_runners(registry)
