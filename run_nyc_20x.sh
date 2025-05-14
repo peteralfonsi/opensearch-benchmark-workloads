a840bf36fc4a21cb1ced584328bdcd3b68328833
Peter Alfonsi <petealft@amazon.com>
2025-05-13 17:01:07 -0700

    fix
    Signed-off-by: Peter Alfonsi <petealft@amazon.com>

--- run_nyc_20x.sh
+++ run_nyc_20x.sh
@@ -1,3 +1,4 @@
 for run in {1..20}; do
-  opensearch-benchmark execute-test --pipeline=benchmark-only --workload-path=/home/ec2-user/osb/opensearch-benchmark-workloads/nyc_taxis --target-host=http://localhost:9200 --throughput-percentiles 0,25,50,75,100 --latency-percentiles 0,10,20,25,30,40,50,60,70,75,80,90,99,99.9,99.99,100
+  echo "STARTING RUN $run \n\n" 
+  opensearch-benchmark execute-test --pipeline=benchmark-only --workload-path=/home/ec2-user/osb/opensearch-benchmark-workloads/nyc_taxis --target-host=http://localhost:9200 --throughput-percentiles 0,25,50,75,100 --latency-percentiles 0,10,20,25,30,40,50,60,70,75,80,90,99,99.9,99.99,100 --kill-running-processes
 done
